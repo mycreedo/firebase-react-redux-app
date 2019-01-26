351ca68bea2d8a013bd83c4d5c9412c547f8c951
myCreedo <myCreedo98@gmail.com>
2019-01-26 14:31:44 +0100

    Added fix for "Provider"

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './store/reducers/rootReducer'
 import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore'
